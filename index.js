c714edea8a2eac9790048ff78a3f42b747caa022
andrew <ammassey42@gmail.com>
2022-10-31 17:45:17 -0700

    function work

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const inquirer = require("inquirer");
 const mysql = require('mysql2');
+const sqltbl = require("console.table");
+
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL Username
+      user: 'root',
+      //Add MySQL Password
+      password: 'password',
+      database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+  );
+  
 
 const whatToDoFunction = ()=>{
     inquirer.prompt([
@@ -7,24 +22,99 @@ const whatToDoFunction = ()=>{
             type: "list",
             name: "whatToDo",
             message: "What would you like to do?",
-            choices: ['View all departments', "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
+            choices: ['View all departments', 
+            "View all roles", 
+            "View all employees", 
+            "Add a department", 
+            "Add a role", 
+            "Add an employee", 
+            "Update an employee role",
+            "Quit"]
         }
-    ]).then(toDoInput =>{
-        const {whatToDo}= toDoInput;
-        if(whatToDO === "View all departments"){
-            //goes to view all departments function
-        } else if(whatToDo === "View all roles"){
-            //goes to view all roles function
-        } else if (whatToDo === "View all employees"){
-            //Goes to view all employees function
-        } else if(whatToDo === "Add a department"){
-            //goes to add a department function
-        } else if (whatToDo === "Add a role"){
-            //goes to add a role function
-        }else if (whatToDo ==="Add an employee"){
-            //goes to add an employee function
-        }else{
-            //goes to update an employee role function
+    ]).then(answers =>{
+        switch (answers.whatToDo) {
+            case 'View all departments':
+                viewDepartment();
+                break;
+
+            case "View all roles":
+                viewRoles();
+                break;
+
+            case "View all employees":
+                viewEmployees();
+                break;
+
+            case "Add a department":
+                addDepartment();
+                break;
+
+            //case  "Add a role":
+                //addRole();
+                //break;
+
+            //case "Add an employee":
+                //addEmployee();
+                //break;
+
+            //case "Update an employee role":
+                //updateEmployee();
+                //break;
+            
+            case "Quit":
+                break;
         }
     })
+};
+
+//views departments
+const viewDepartment= ()=>{
+    db.query("SELECT * FROM departments", function (err, results){
+        console.table(results);
+        whatToDoFunction();
+    })
+};
+
+//views roles
+const viewRoles = ()=>{
+    db.query("SELECT * FROM roles JOIN departments ON roles.department_id=departments.id", function(err, results){
+        console.table(results);
+        whatToDoFunction();
+    })
+};
+
+//views all employees
+const viewEmployees = ()=>{
+    db.query("SELECT * FROM employees", function(err, results){
+        console.table(results);
+        whatToDoFunction();
+    })
+};
+
+//adds another department to the table
+const addDepartment = ()=>{
+    inquirer.prompt([
+    {
+        type: "input",
+        name:"departmentName",
+        message:"What Department do you want to add?"
+    },
+    ]).then((response)=>{
+        db.query("INSERT INTO DEPARTMENTS SET ?", {name: response. departmentName}, function(err, results){
+            console.table(results);
+            whatToDoFunction();
+        })
+    })
 }
+
+whatToDoFunction();
+
+
+// TODO:WHEN I choose to view all employees
+// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+// TODO:WHEN I choose to add a role
+// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+// TODO:WHEN I choose to add an employee
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+// TODO:WHEN I choose to update an employee role
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,24 +1,30 @@
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
+USE employees_db;
 
-CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,3) NOT NULL,
     department_id INT NOT NULL,
-    PRIMARY KEY (id)
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE CASCADE
 );
-CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NOT NULL,
-    PRIMARY KEY (id)
-);
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE CASCADE,
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
+    );
